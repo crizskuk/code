918e42f9462e2c93a9af6c9d09aa06a1f3033496
cristian kook <crizskuk@gmail.com>
2022-03-12 20:18:06 -0500

    update pointer_funct

--- function_pointer.cpp
+++ function_pointer.cpp
@@ -12,15 +12,18 @@ void PrintEdades(uint16_t x) {
 
 void BucleForEach(std::vector<uint16_t>& e, void(*func)(uint16_t)) {
 	for (auto& x : e) {
-		PrintEdades(x);
+		func(x);
 	}
 }
 
 int main(void) {
-	//uint16_t(*funct)(uint16_t) = Edad;
-	//std::cout << "edad is: " << funct(5) << std::endl;
+	uint16_t(*funct)(uint16_t) = Edad;
+	std::cout << "edad is: " << funct(5) << std::endl;
 	std::vector<uint16_t> v1 = { 2,4,8,16,32,64 };
 	void(*punt)(uint16_t) = PrintEdades;
 	BucleForEach(v1,punt);
+	BucleForEach(v1, [](uint16_t x) 
+		{
+			std::cout << "valor: " << x << std::endl; });
 	return 0;
 }
